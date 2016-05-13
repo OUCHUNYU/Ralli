75e1a2d6d82e524d8d482583fd3f33ce129f7646
OUCHUNYU <ouchunyu@yahoo.com>
2016-05-13 15:40:05 -0700

    make app dir and set firebase connect

---
+++ App/Utils/usersApi.js
@@ -0,0 +1,14 @@
+const Firebase = require('firebase');
+
+const FirebaseUsersUrl = 'https://ralli.firebaseio.com/users';
+
+const UsersRef = new Firebase(FirebaseUsersUrl);
+
+
+var usersApi = {
+  createUser = function() {
+
+  }
+}
+
+module.exports = usersApi;
