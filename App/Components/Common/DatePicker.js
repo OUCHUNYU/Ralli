3d6e84e09c93d677840cc74da2ca15b7f2acb490
Eric Gumerlock <eric.gumerlock@gmail.com>
2016-05-15 12:23:53 -0700

    Create create marker page, add date picker, issue with Start Rally link

---
+++ App/Components/Common/DatePicker.js
@@ -0,0 +1,134 @@
+var React = require('react');
+var ReactNative = require('react-native');
+var {
+  DatePickerIOS,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} = ReactNative;
+
+var DatePicker = React.createClass({
+  getDefaultProps: function () {
+    return {
+      date: new Date(),
+      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      date: this.props.date,
+      timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
+    };
+  },
+
+  onDateChange: function(date) {
+    this.setState({date: date});
+  },
+
+  onTimezoneChange: function(event) {
+    var offset = parseInt(event.nativeEvent.text, 10);
+    if (isNaN(offset)) {
+      return;
+    }
+    this.setState({timeZoneOffsetInHours: offset});
+  },
+
+  render: function() {
+    // Ideally, the timezone input would be a picker rather than a
+    // text input, but we don't have any pickers yet :(
+    return (
+      <View>
+        <WithLabel label="Value:">
+          <Text>{
+            this.state.date.toLocaleDateString() +
+            ' ' +
+            this.state.date.toLocaleTimeString()
+          }</Text>
+        </WithLabel>
+        <WithLabel label="Timezone:">
+          <TextInput
+            onChange={this.onTimezoneChange}
+            style={styles.textinput}
+            value={this.state.timeZoneOffsetInHours.toString()}
+          />
+          <Text> hours from UTC</Text>
+        </WithLabel>
+        <DatePickerIOS
+          date={this.state.date}
+          mode="datetime"
+          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+          onDateChange={this.onDateChange}
+        />
+      </View>
+    );
+  },
+});
+
+var WithLabel = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.labelContainer}>
+        <View style={styles.labelView}>
+          <Text style={styles.label}>
+            {this.props.label}
+          </Text>
+        </View>
+        {this.props.children}
+      </View>
+    );
+  }
+});
+
+var Heading = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>
+          {this.props.label}
+        </Text>
+      </View>
+    );
+  }
+});
+
+module.exports = DatePicker;
+exports.examples = [
+{
+  title: '<DatePickerIOS>',
+  render: function(): ReactElement {
+    return <DatePickerExample />;
+  },
+}];
+
+var styles = StyleSheet.create({
+  textinput: {
+    height: 26,
+    width: 50,
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    padding: 4,
+    fontSize: 13,
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 2,
+  },
+  labelView: {
+    marginRight: 10,
+    paddingVertical: 2,
+  },
+  label: {
+    fontWeight: '500',
+  },
+  headingContainer: {
+    padding: 4,
+    backgroundColor: '#f6f7f8',
+  },
+  heading: {
+    fontWeight: '500',
+    fontSize: 14,
+  },
+});
