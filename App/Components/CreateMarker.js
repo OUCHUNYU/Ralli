1c74eeeb4cba86b9fe591715aa23cf48985000d0
OUCHUNYU <ouchunyu@yahoo.com>
2016-05-16 09:50:26 -0700

    delete mappage

--- App/Components/CreateMarker.js
+++ App/Components/CreateMarker.js
@@ -1,5 +1,5 @@
 var Button = require('./Common/button');
-var MapPage = require('./MapPage');
+
 var DatePicker = require('./Common/DatePicker');
 var GroupsInvitePage = require('./GroupsInvitePage');
 
