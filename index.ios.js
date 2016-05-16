638fece9458075d607ca079d1c96fac0bc60645e
Eric Gumerlock <eric.gumerlock@gmail.com>
2016-05-16 15:50:24 -0700

    Change index route

--- index.ios.js
+++ index.ios.js
@@ -27,7 +27,7 @@ class Ralli extends Component {
       style={styles.wrapper}
       initialRoute = {{
         title: 'Rally',
-        component: GoogleMap
+        component: LoginPage
       }} />
     );
   }
