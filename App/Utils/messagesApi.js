f4293ee4fba7687b1f52da28d644f41d69396158
OUCHUNYU <ouchunyu@yahoo.com>
2016-05-18 12:01:20 -0700

    Add individual feed

--- App/Utils/messagesApi.js
+++ App/Utils/messagesApi.js
@@ -6,15 +6,19 @@ var messagesApi = {
   chatRoomMessenger: function(newMemberId, groupName) {
     var feedMessage = "You have now been added to the group named: " + groupName;
     new Firebase(FBUrl + "users/" + newMemberId + "/feed").once("value").then((res) => {
-      if(res) {
+      if(res.val()) {
         var feedArr = res.val();
+        console.log(feedArr)
         feedArr.push({desc: feedMessage})
-        new Firebase(FBUrl + "users/" + newMemberId + "/feed").update({feed: feedArr})
+        new Firebase(FBUrl + "users/" + newMemberId).update({feed: feedArr})
       }else {
-        new Firebase(FBUrl + "users/" + newMemberId + "/feed").push({feed: [{desc: feedMessage}]})
+        new Firebase(FBUrl + "users/" + newMemberId).update({feed: [{desc: feedMessage}]})
       }
     })
   }
 }
 
 module.exports = messagesApi;
+
+
+messagesApi.chatRoomMessenger("-KI4AVqDgmxvxsmVo4dD", "Awesome group")
